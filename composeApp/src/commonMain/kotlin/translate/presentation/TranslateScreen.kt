5b06ca2f8d159aebca952e35ea471997b7ac6282
mrThinBone <vinh.tv2003@gmail.com>
2024-03-13 22:25:17 +0700

    UI: history list

--- composeApp/src/commonMain/kotlin/translate/presentation/TranslateScreen.kt
+++ composeApp/src/commonMain/kotlin/translate/presentation/TranslateScreen.kt
@@ -1,33 +1,80 @@
+@file:OptIn(ExperimentalResourceApi::class)
+
 package translate.presentation
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.FabPosition
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.vectorResource
+import translate.domain.translate.TranslateError
 import translate.presentation.components.LanguageDropDown
 import translate.presentation.components.SwapLanguagesButton
+import translate.presentation.components.TranslateHistoryItem
 import translate.presentation.components.TranslateTextField
+import translator.composeapp.generated.resources.Res
+import translator.composeapp.generated.resources.mic
 
 @Composable
 fun TranslateScreen(
     state: State<TranslateState>,
     onEvent: (TranslateEvent) -> Unit
 ) {
+    val context = LocalContext.current
+
+    LaunchedEffect(key1 = state.value.error) {
+        val message = when(state.value.error) {
+            TranslateError.SERVICE_UNAVAILABLE -> "error_service_unavailable"
+            TranslateError.CLIENT_ERROR -> "client_error"
+            TranslateError.SERVER_ERROR -> "server_error"
+            TranslateError.UNKNOWN_ERROR -> "unknown_error"
+            else -> null
+        }
+        message?.let {
+            Toast.makeText(context, it, Toast.LENGTH_LONG).show()
+            onEvent(TranslateEvent.OnErrorSeen)
+        }
+    }
+
     Scaffold(
         floatingActionButton = {
-
-        }
+            FloatingActionButton(
+                onClick = {
+                    onEvent(TranslateEvent.RecordAudio)
+                },
+                backgroundColor = MaterialTheme.colors.primary,
+                contentColor = MaterialTheme.colors.onPrimary,
+                modifier = Modifier.size(75.dp)
+            ) {
+                Icon(
+                    imageVector = vectorResource(Res.drawable.mic),
+                    contentDescription = "record audio"
+                )
+            }
+        },
+        floatingActionButtonPosition = FabPosition.End
     ) { padding ->
         LazyColumn(
             modifier = Modifier
@@ -98,14 +145,12 @@ fun TranslateScreen(
                                 append(text)
                             }
                         )
-                        // TODO: show toast
-                        /*Toast.makeText(
+                        // TODO: string as resource?
+                        Toast.makeText(
                             context,
-                            context.getString(
-                                com.plcoding.translator_kmm.android.R.string.copied_to_clipboard
-                            ),
+                            "Text copied to clipboard",
                             Toast.LENGTH_LONG
-                        ).show()*/
+                        ).show()
                     },
                     onCloseClick = {
                         onEvent(TranslateEvent.CloseTranslation)
@@ -119,6 +164,24 @@ fun TranslateScreen(
                     modifier = Modifier.fillMaxWidth()
                 )
             } // second item block
+            item {
+                if (state.value.history.isNotEmpty()) {
+                    Text(
+                        text = "History",
+                        style = MaterialTheme.typography.h2
+                    )
+                }
+            }
+
+            items(state.value.history) { item ->
+                TranslateHistoryItem(
+                    item = item,
+                    onClick = {
+                        onEvent(TranslateEvent.SelectHistoryItem(item))
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
         }
     }
 }
